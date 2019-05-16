26f7f902b78576f4f364f69d0edfab4622eb16aa
Soren <S_Gravesen92@hotmail.com>
2019-05-17 00:57:52 +0200

    (grav) Move files into src-folder

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Header from './components/shared/Header';
-import Footer from './components/shared/Footer';
-import Frontpage from './components/single_pages/frontpage/Frontpage';
-import Base from './components/shared/base';
+import Header from './src/components/shared/Header';
+import Footer from './src/components/shared/Footer';
+import Frontpage from './src/components/single_pages/frontpage/Frontpage';
+import Base from './src/components/shared/base';
 import "babel-polyfill";
 
 class App extends Component{
