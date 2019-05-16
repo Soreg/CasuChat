74c7b3c42b688271f34aa3a83dd9e0ff6f4c66e0
Soren <S_Gravesen92@hotmail.com>
2019-05-17 01:51:03 +0200

    (grav) implement basic firebase

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,20 @@
+import app from 'firebase/app';
+
+const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  };
+
+  class Firebase {
+    constructor() {
+      if (!app.apps.length) {
+        app.initializeApp(config);
+      }
+    }
+  }
+
+  export default Firebase;
