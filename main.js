c02b01557b2a480ec1194047dafde61a6d8057e5
Soren <S_Gravesen92@hotmail.com>
2019-05-17 01:21:53 +0200

    (grav) add basic route

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/components/app.js';
+import App from './src/components/App/index.js';
 
 ReactDOM.render(<App />, document.getElementById('app'));
