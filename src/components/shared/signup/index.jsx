93cedb7b0991c09938bf3c8fe3f6fce84b9de18e
Soren <S_Gravesen92@hotmail.com>
2019-05-17 01:52:47 +0200

    (grav) removed old base.js for firebase

--- src/components/shared/signup/index.jsx
+++ src/components/shared/signup/index.jsx
@@ -1,25 +1,24 @@
 import React, { Component } from "react";
-import base from "../../shared/base";
 
 import SignUpView from "./SignUpView";
 
 class SignUpContainer extends Component {
     handleSignUp = async(e, username, email, password, date) => {
-        e.preventDefault();
-        try {
-            const user = await base
-            .auth()
-            .createUserWithEmailAndPassword(email, password);
-            base.auth().onAuthStateChanged(function(user) {
-                if (user) {
-                  user.updateProfile({
-                    displayName: username,
-                  });     
-                }
-            })
-        } catch (error) {
-            alert(error);
-        }
+        // e.preventDefault();
+        // try {
+        //     const user = await base
+        //     .auth()
+        //     .createUserWithEmailAndPassword(email, password);
+        //     base.auth().onAuthStateChanged(function(user) {
+        //         if (user) {
+        //           user.updateProfile({
+        //             displayName: username,
+        //           });     
+        //         }
+        //     })
+        // } catch (error) {
+        //     alert(error);
+        // }
     };
 
     render() {
