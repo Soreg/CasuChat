b146994e3358ff90097da376b19b2288898d410e
Soren <S_Gravesen92@hotmail.com>
2019-05-17 23:37:15 +0200

    (grav) Add auth-state to index file

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
-import {
-   BrowserRouter as Router,
-   Route,
- } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
+import { withFirebase } from '../Firebase';
 import Frontpage from '../single_pages/frontpage/Frontpage';
 import AccountPage from '../Account/';
 
 class App extends Component{
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         authUser: null
+      };
+   }
+
+   componentDidMount() {
+      this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+        authUser
+          ? this.setState({ authUser })
+          : this.setState({ authUser: null });
+      });
+    }
+
+    componentWillUnmount() {
+      this.listener();
+    }
+
    render(){
       return(
          <div>
              <Router>
-             <Route exact path={ROUTES.LANDING} component={Frontpage} />
-             <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+             <Route exact path={ROUTES.LANDING} component={Frontpage} authUser={this.state.authUser} />
+             <Route path={ROUTES.ACCOUNT} component={AccountPage} authUser={this.state.authUser} />
              </Router>
          </div>
       );
    }
 }
-export default App;
+export default withFirebase(App);
