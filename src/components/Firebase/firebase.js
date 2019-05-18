3a9464704655d414d0795f58a13886ec72d1c842
Soren <S_Gravesen92@hotmail.com>
2019-05-19 01:32:27 +0200

    (grav) add database for dev + prod

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,36 +1,48 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 
-const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  };
+const devConfig = {
+  apiKey: process.env.REACT_APP_DEV_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_DEV_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_DEV_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_DEV_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_DEV_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_DEV_FIREBASE_SENDER_ID,
+};
 
-  class Firebase {
-    constructor() {
-      if (!app.apps.length) {
-        app.initializeApp(config);
-      }
+const prodConfig = {
+  apiKey: process.env.REACT_APP_PROD_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_PROD_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_PROD_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_PROD_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_PROD_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_PROD_FIREBASE_SENDER_ID,
+};
 
-      this.auth = app.auth();
+const config =
+process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
+class Firebase {
+  constructor() {
+    if (!app.apps.length) {
+      app.initializeApp(config);
     }
 
-    doCreateUserWithEmailAndPassword = (email, password) =>
-    this.auth.createUserWithEmailAndPassword(email, password);
-
-    doSignInWithEmailAndPassword = (email, password) =>
-    this.auth.signInWithEmailAndPassword(email, password);
-
-    doSignOut = () => this.auth.signOut();
-
-    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
-
-    doPasswordUpdate = password =>
-      this.auth.currentUser.updatePassword(password);
+    this.auth = app.auth();
   }
 
-  export default Firebase;
+  doCreateUserWithEmailAndPassword = (email, password) =>
+  this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+  this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
