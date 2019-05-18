e299a822a5aa34540911b1864d9265b81145e592
Soren <S_Gravesen92@hotmail.com>
2019-05-19 01:57:38 +0200

    (grav) Add proper server paths

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'bundle')));
 app.set('port', process.env.PORT || 8080);
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'bundle', 'index.html'));
+});
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
